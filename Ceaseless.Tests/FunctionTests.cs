8ec5cae8b224c3e693604dde2463a4fe4f4bdc1d
Adrian Godong <adrian.godong@gmail.com>
2017-07-18 20:31:44 -0700

    First commit.

---
+++ Ceaseless.Tests/FunctionTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Amazon.Lambda.Core;
+
+namespace Ceaseless.Tests
+{
+    [TestClass]
+    public class FunctionTests
+    {
+
+        private Mock<IEngine> mockEngine;
+        private Mock<ILambdaContext> mockLambdaContext;
+        private Mock<ILambdaLogger> mockLambdaLogger;
+        private Function function;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mockEngine = new Mock<IEngine>();
+            mockLambdaContext = new Mock<ILambdaContext>();
+            mockLambdaLogger = new Mock<ILambdaLogger>();
+
+            mockLambdaContext.Setup(mock => mock.Logger).Returns(mockLambdaLogger.Object);
+
+            function = new Function(mockEngine.Object);
+        }
+
+        [TestMethod]
+        public async Task HandleAddPersonToPrayIntent_ShouldCollectInput_CallAddPersonToPray_ReturnSkillResponse()
+        {
+            // Arrange
+            var userId = Guid.NewGuid().ToString("N");
+            var name = Guid.NewGuid().ToString("N");
+
+            var skillRequest = new SkillRequest
+            {
+                Session = new Session
+                {
+                    User = new User
+                    {
+                        UserId = userId
+                    }
+                }
+            };
+
+            var intentRequest = new IntentRequest
+            {
+                Intent = new Intent
+                {
+                    Slots = new Dictionary<string, Slot>
+                    {
+                        { "Name", new Slot { Value = name } }
+                    }
+                }
+            };
+
+            // Act
+            var response = await function.HandleAddPersonToPrayIntent(skillRequest, intentRequest, mockLambdaContext.Object);
+
+            // Assert
+            mockEngine.Verify(mock => mock.AddPersonToPray(userId, name), Times.Once);
+            Assert.IsNotNull(response);
+            Assert.IsNotNull(response.Response.OutputSpeech);
+        }
+
+        [TestMethod]
+        public async Task HandleGetNextPersonToPrayIntent_ShouldCollectInput_CallGetPeopleToPray_ReturnSkillResponse()
+        {
+            // Arrange
+            var userId = Guid.NewGuid().ToString("N");
+            var name = Guid.NewGuid().ToString("N");
+
+            var skillRequest = new SkillRequest
+            {
+                Session = new Session
+                {
+                    User = new User
+                    {
+                        UserId = userId
+                    }
+                }
+            };
+
+            mockEngine.Setup(mock => mock.GetPeopleToPray(userId)).ReturnsAsync(new List<string> { name });
+
+            // Act
+            var response = await function.HandleGetNextPersonToPrayIntent(skillRequest, null, mockLambdaContext.Object);
+
+            // Assert
+            mockEngine.Verify(mock => mock.GetPeopleToPray(userId), Times.Once);
+            Assert.IsNotNull(response);
+            Assert.IsNotNull(response.Response.OutputSpeech);
+        }
+
+        [TestMethod]
+        public async Task HandleGetNextPersonToPrayIntent_ShouldCollectInput_CallGetPeopleToPray_ReturnSkillResponse_WhenEmpty()
+        {
+            // Arrange
+            var userId = Guid.NewGuid().ToString("N");
+
+            var skillRequest = new SkillRequest
+            {
+                Session = new Session
+                {
+                    User = new User
+                    {
+                        UserId = userId
+                    }
+                }
+            };
+
+            mockEngine.Setup(mock => mock.GetPeopleToPray(userId)).ReturnsAsync(new List<string>());
+
+            // Act
+            var response = await function.HandleGetNextPersonToPrayIntent(skillRequest, null, mockLambdaContext.Object);
+
+            // Assert
+            mockEngine.Verify(mock => mock.GetPeopleToPray(userId), Times.Once);
+            Assert.IsNotNull(response);
+            Assert.IsNotNull(response.Response.OutputSpeech);
+        }
+
+    }
+}
---
+++ Ceaseless/Function.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using AlexaLambdaHandler;
+using Amazon.Lambda.Core;
+using Amazon.Runtime;
+using Newtonsoft.Json;
+
+[assembly: InternalsVisibleTo("Ceaseless.Tests")]
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+namespace Ceaseless
+{
+    public class Function : LambdaHandler
+    {
+
+        private readonly IEngine engine;
+
+        public Function() :
+            this(new Engine(
+                new EnvironmentVariablesAWSCredentials(),
+                new EnvironmentVariableAWSRegion().Region))
+        {
+        }
+
+        internal Function(IEngine engine)
+        {
+            this.engine = engine;
+
+            AddAsyncIntentRequestHandler("AddPersonToPray", HandleAddPersonToPrayIntent);
+            AddAsyncIntentRequestHandler("GetNextPersonToPray", HandleGetNextPersonToPrayIntent);
+
+            DefaultIntentRequestHandler = (request, intentRequest, context) => Task.FromResult(ResponseBuilder.Tell(
+                new PlainTextOutputSpeech
+                {
+                    Text = "I don't understand what you are asking."
+                }));
+
+            SystemExceptionRequestHandler = (request, exceptionRequest, context) =>
+            {
+                context.Logger.Log($"ERROR: {exceptionRequest.Error.Message}");
+                return Task.FromResult(ResponseBuilder.Empty());
+            };
+        }
+
+        public async Task<SkillResponse> FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            context.Logger.Log($"Input: {JsonConvert.SerializeObject(input)}");
+            return await Handle(input, context);
+        }
+
+        internal async Task<SkillResponse> HandleAddPersonToPrayIntent(SkillRequest request, IntentRequest intentRequest, ILambdaContext context)
+        {
+            var userId = request.Session.User.UserId;
+            var name = intentRequest.Intent.Slots["Name"].Value;
+            context.Logger.Log($"{userId}: Adding '{name}'");
+            await engine.AddPersonToPray(userId, name);
+            return ResponseBuilder.Tell(new PlainTextOutputSpeech() { Text = $"I have added {name} to your prayer list." });
+        }
+
+        internal async Task<SkillResponse> HandleGetNextPersonToPrayIntent(SkillRequest request, IntentRequest intentRequest, ILambdaContext context)
+        {
+            var userId = request.Session.User.UserId;
+
+            var names = await engine.GetPeopleToPray(userId);
+            if (names.Count == 0)
+            {
+                return ResponseBuilder.Tell(new PlainTextOutputSpeech() { Text = "You have no one to pray in your list." });
+            }
+
+            var name = names[new Random().Next(names.Count)];
+            return ResponseBuilder.Tell(new PlainTextOutputSpeech() { Text = $"The next person to pray for is {name}." });
+        }
+
+    }
+}
